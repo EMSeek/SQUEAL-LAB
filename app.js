981b37d9c67886358114d30bda9b7ffe68601483
EMSeek <emarcussen@seek.com.au>
2024-10-07 12:06:39 +1100

    Routes behave correctly

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require('./db');
 const app = express();
 const port = 3000;
 
+app.use('/files', express.static('files'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -26,26 +27,36 @@ app.get('/route1/:id', (req, res) => {
 // Route 2: POST /route2
 app.post('/route2', (req, res) => {
   const id = req.body.id;
-  console.log("SELECT * FROM posts WHERE id = " + id + "["+req.body+"]");
   db.get("SELECT * FROM posts WHERE id = " + id, (err, row) => {
     if (err) {
-      res.status(500).json({ error: err.message });
+      res.status(500).json({ error: "there was an error" });
       return;
     }
-    res.json(row);
+    res.status(200).json({ found: "post exists" });
+    return;
   });
 });
 
 // Route 3: POST /route3
 app.post('/route3', (req, res) => {
-  const id = req.body.id;
+  const id = req.body.id.replace(/ /, '');
   db.get("SELECT * FROM posts WHERE id = "+id, (err, row) => {
     if (err) {
-      res.status(500).json({ error: err.message });
-      return;
+      console.log("Error: "+err.message)
     }
-    res.json(row);
+      var fs = require('fs');
+      var dataToWrite = JSON.stringify(row)
+      
+      fs.writeFile('files/export.json', dataToWrite, 'utf8', function (err) {
+        if (err) {
+          console.log('Some error occured - file either not saved or corrupted file saved.');
+        } else{
+          console.log('It\'s saved!');
+        }
+    });
   });
+  res.status(200).json({ constant: "constant response" });
+  return;
 });
 
 app.listen(port, () => {
